0713c7c74461aac6319b9593185e9e84f3866776
Morten Kjetland <mbk@kjetland.com>
2012-01-13 21:09:27 +0100

    fixed bug related to get rendering result as string and extends

--- src/main/java/play/template2/GTRenderingResult.java
+++ src/main/java/play/template2/GTRenderingResult.java
@@ -1,7 +1,9 @@
 package play.template2;
 
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,11 +35,13 @@ public class GTRenderingResult {
      * @return the rendering-output as string
      */
     public String getAsString() {
-        StringBuilder sb = new StringBuilder();
-        for ( StringWriter s : allOuts) {
-            sb.append( s.toString());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        writeOutput(out, "utf-8");
+        try {
+            return out.toString("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
         }
-        return sb.toString();
     }
 
 }
