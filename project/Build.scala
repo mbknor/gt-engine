f21b63e51ce51015538953648983c7bd9a280a91
Morten Kjetland <mbk@kjetland.com>
2012-01-19 22:25:09 +0100

    updated project to have dependency to java compiler via maven

--- project/Build.scala
+++ project/Build.scala
@@ -3,6 +3,8 @@ import Keys._
 
 object GTEngineBuild extends Build {
 
+  val mbknorGithubRepoUrl = "http://mbknor.github.com/m2repo/releases/"
+
   lazy val GTEngineProject = Project(
     "gt-engine",
     new File("."),
@@ -12,25 +14,28 @@ object GTEngineBuild extends Build {
                 publishTo := Some(Resolvers.mbknorRepository),
                 scalacOptions ++= Seq("-Xlint","-deprecation", "-unchecked","-encoding", "utf8"),
                 javacOptions ++= Seq("-encoding", "utf8", "-g"),
-                resolvers ++= Seq(DefaultMavenRepository)
+                resolvers ++= Seq(DefaultMavenRepository, Resolvers.mbknorGithubRepo)
             )
         )
 
 
   object Resolvers {
-      val mbknorRepository = Resolver.ssh("my local mbknor repo", "localhost", "~/projects/mbknor.github.com/m2repo/releases/")(Resolver.mavenStylePatterns)
+    val mbknorRepository = Resolver.ssh("my local mbknor repo", "localhost", "~/projects/mbknor.github.com/m2repo/releases/")(Resolver.mavenStylePatterns)
+    val mbknorGithubRepo = "mbknor github Repository" at mbknorGithubRepoUrl
   }
 
   object Dependencies {
 
       val runtime = Seq(
         "org.codehaus.groovy" % "groovy" % "1.8.5",
+        "kjetland" % "org_eclipse_jdt_core" % "3.8.0.v_C03",
         	"commons-collections" % "commons-collections" % "3.2.1",
         	"commons-lang" % "commons-lang" % "2.6",
         	"org.easytesting" % "fest-assert" % "1.4" % "test",
         	"com.novocode" % "junit-interface" % "0.7" % "test"
       )
   }
+
 
 
   object BuildSettings {
