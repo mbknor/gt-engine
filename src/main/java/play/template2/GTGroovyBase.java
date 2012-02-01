53e2fabdd9b464a152e82655234ec3cc292f1dd3
Morten Kjetland <mbk@kjetland.com>
2012-02-01 21:29:01 +0100

    now %{ print 'x' }% ends up in the right place - fixes #1

--- src/main/java/play/template2/GTGroovyBase.java
+++ src/main/java/play/template2/GTGroovyBase.java
@@ -4,6 +4,8 @@ import groovy.lang.MissingPropertyException;
 import groovy.lang.Script;
 import play.template2.exceptions.GTException;
 
+import java.io.PrintWriter;
+
 public class GTGroovyBase extends Script {
 
     @Override
@@ -32,5 +34,42 @@ public class GTGroovyBase extends Script {
     
     protected Class _resolveClass(String clazzName) {
         throw new GTException("Not implemented by default. Must be overridden by framework impl");
+    }
+
+    // Returns the correct PrintWriter right now
+    private PrintWriter getPrintWriter() {
+        // if someone has given us an alternative out (PrintWriter), then we must write the result to that PrintWriter.
+        PrintWriter out = (PrintWriter)getProperty("out");
+        if (out == null) {
+            // Create PrintWriter that prints to our StringWriter
+            GTJavaBase javaBase = (GTJavaBase)  getProperty("java_class");
+            out = new PrintWriter(javaBase.out, true); // autoflush
+        }
+        return out;
+    }
+
+    @Override
+    public void println() {
+        getPrintWriter().println();
+    }
+
+    @Override
+    public void print(Object value) {
+        getPrintWriter().print(value);
+    }
+
+    @Override
+    public void println(Object value) {
+        getPrintWriter().println(value);
+    }
+
+    @Override
+    public void printf(String format, Object value) {
+        getPrintWriter().printf(format, value);
+    }
+
+    @Override
+    public void printf(String format, Object[] values) {
+        getPrintWriter().printf(format, values);
     }
 }
--- src/test/java/play/template2/OutOveridingTest.java
+++ src/test/java/play/template2/OutOveridingTest.java
@@ -5,15 +5,21 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 public class OutOveridingTest {
 
     @Test
-    public void testLegacyOutOverriding() {
-        TemplateSourceRenderer r = new TemplateSourceRenderer( new GTTemplateRepoBuilder().build());
+    public void testLegacyOutOverriding() throws Exception {
+        TemplateSourceRenderer sr = new TemplateSourceRenderer( new GTTemplateRepoBuilder().build());
 
         Map<String, Object> args = new HashMap<String, Object>();
         args.put("myData", "xxx");
-        
+
+        assertThat( sr.renderSrc("a%{ print 'b' }%c", args) ).isEqualTo("abc");
+        assertThat( sr.renderSrc("a%{ println('b') }%c", args) ).isEqualTo("ab\nc");
+        assertThat( sr.renderSrc("a%{ printf('%d',1) }%c", args) ).isEqualTo("a1c");
+
 
     }
 }
