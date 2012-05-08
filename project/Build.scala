00c77cc918090d5bc8c4a6ef9b3af0de80fc26fa
Morten Kjetland <mbk@kjetland.com>
2012-05-08 20:48:29 +0200

    Release 0.2

--- project/Build.scala
+++ project/Build.scala
@@ -42,7 +42,7 @@ object GTEngineBuild extends Build {
   object BuildSettings {
 
           val buildOrganization = "kjetland"
-          val buildVersion      = "0.1.7.14-SNAPSHOT"
+          val buildVersion      = "0.2"
           val buildScalaVersion = "2.9.1"
           val buildSbtVersion   = "0.11.2"
 
